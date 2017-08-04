f20f8ec05f9e1bdd4ea675f90ab9e62a48be7371
Dom Tuffrey <dominict98@hotmail.com>
2017-08-04 14:38:51 +0100

    Add files via upload

---
+++ party time.js
@@ -0,0 +1,30 @@
+function epilepticFit(){
+	for(var i=0; i<100; i++){
+		document.getElementById('domIsAWasteman').style.backgroundColor = 'red';
+		console.log('as;ldkfj')
+		setTimeout(10000);
+		document.getElementById('domIsAWasteman').style.backgroundColor = 'blue';
+		console.log('asdlfk222')
+		setTimeout(10000);
+	}
+}
+
+function Yellow(){
+	document.getElementById('domIsAWasteman').style.backgroundColor = 'yellow';
+}
+
+function Red(){
+	document.getElementById('domIsAWasteman').style.backgroundColor = 'red';
+}
+
+function Green(){
+	document.getElementById('domIsAWasteman').style.backgroundColor = 'green';
+}
+
+function Blue(){
+	document.getElementById('domIsAWasteman').style.backgroundColor = 'blue';
+}
+
+function originalBlue(){
+	document.getElementById('domIsAWasteman').style.backgroundColor = '#D2FFFE';
+}
